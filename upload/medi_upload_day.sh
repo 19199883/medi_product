728a6f23fd318213a997027c406de347f407c1d2
wangying <17199883@qq.com>
2021-01-22 18:02:15 +0800

    ddd

---
+++ upload/medi_upload_day.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+# the directory where this script file is.
+function enter_cur_dir(){
+         this_dir=`pwd`
+         dirname $0|grep "^/" >/dev/null
+         if [ $? -eq 0 ]
+         then
+                         this_dir=`dirname $0`
+         else
+                         dirname $0|grep "^\." >/dev/null
+                         retval=$?
+                         if [ $retval -eq 0 ]
+                         then
+                                         this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+                         else
+                                         this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+                         fi
+         fi
+
+        cd $this_dir
+}
+
+function upload_files
+{
+	rm -r upload_so
+	unzip upload.zip
+	cp -a upload_so/* ./upload/
+	
+	######### dce #########
+	sh	/home/medi/upload/dce/day067/startup.sh
+	sh	/home/medi/upload/dce/day115/startup.sh
+	sh	/home/medi/upload/dce/day210/startup.sh
+
+	######### zce #########
+	sh	/home/medi/upload/zce/day21-00/startup.sh
+	sh	/home/medi/upload/zce/day211/startup.sh
+	sh	/home/medi/upload/zce/day225/startup.sh
+
+	######### shfe #########
+	sh	/home/medi/upload/shfe/day110/startup.sh
+	sh	/home/medi/upload/shfe/day169/startup.sh
+	sh	/home/medi/upload/shfe/day96/startup.sh
+}
+
+ 
+enter_cur_dir
+upload_files                                                                                                                                           
+      
---
+++ upload/medi_upload_night.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+# the directory where this script file is.
+function enter_cur_dir(){
+         this_dir=`pwd`
+         dirname $0|grep "^/" >/dev/null
+         if [ $? -eq 0 ]
+         then
+                         this_dir=`dirname $0`
+         else
+                         dirname $0|grep "^\." >/dev/null
+                         retval=$?
+                         if [ $retval -eq 0 ]
+                         then
+                                         this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+                         else
+                                         this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+                         fi
+         fi
+
+        cd $this_dir
+}
+
+function upload_files
+{
+	rm -r upload_so
+	unzip upload.zip
+	cp -a upload_so/* ./upload/
+	
+	######### dce #########
+	sh	/home/medi/upload/dce/night067/startup.sh
+	sh	/home/medi/upload/dce/night115/startup.sh
+	sh	/home/medi/upload/dce/night210/startup.sh
+
+	######### zce #########
+	sh	/home/medi/upload/zce/night21-00/startup.sh
+	sh	/home/medi/upload/zce/night211/startup.sh
+	sh	/home/medi/upload/zce/night225/startup.sh
+
+	######### shfe #########
+	sh	/home/medi/upload/shfe/night110/startup.sh
+	sh	/home/medi/upload/shfe/night169/startup.sh
+	sh	/home/medi/upload/shfe/night96/startup.sh
+}
+
+ 
+enter_cur_dir
+upload_files                                                                                                                                           
+      
