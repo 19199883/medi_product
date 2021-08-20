61f6db2cc0e7e7f1b59b61814e89c4ffbf8c45c8
wangying <17199883@qq.com>
2021-08-20 17:07:13 +0800

    shfe

---
+++ upload/shfe/day96/shfe-trade1/start-hzp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##################
+# 该脚本放在shfe-trade1服务器上，用来启动day96。
+# 由hzp操作.
+# 
+#
+#######################################
+
+
+# the directory where this script file is.
+function enter_cur_dir()
+{
+	 this_dir=`pwd`
+	 dirname $0|grep "^/" >/dev/null
+	 if [ $? -eq 0 ];then
+		 this_dir=`dirname $0`
+	 else
+		 dirname $0|grep "^\." >/dev/null
+		 retval=$?
+		 if [ $retval -eq 0 ];then
+			 this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+		 else
+			 this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+		 fi
+	 fi
+
+	cd $this_dir
+}
+
+enter_cur_dir
+
+echo "------------day96 is starting--------"
+sh start.sh
+echo "------------day96 hase started--------"
+
